0ed8d0070d07924c66e777590d1a8f0e77461be7
MohammadHassan <mhze.uk@gmail.com>
2026-06-21 21:03:29 +0100

    Build bat file updated to auto open godot after successful build

--- Build.bat
+++ Build.bat
@@ -1,4 +1,17 @@
 @echo off
 cd /d D:\GodotSourceCode\godot
-py -3.13 -m SCons platform=windows accesskit=no d3d12=no
-pause
+
+echo Building Godot...
+py -3.13 -m SCons platform=windows dev_mode=yes accesskit=no d3d12=no
+
+if %errorlevel% neq 0 (
+    echo Build failed.
+    pause
+    exit /b
+)
+
+echo Build finished. Launching Godot...
+
+start "" "bin\godot.windows.editor.x86_64.exe"
+
+exit
