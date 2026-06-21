68c4a1d72ed461920e332ba9e6967d489023d975
MohammadHassan <mhze.uk@gmail.com>
2026-06-21 21:30:09 +0100

    clean up build.bat file

--- Build.bat
+++ Build.bat
@@ -1,17 +1,5 @@
-@echo off
 cd /d D:\GodotSourceCode\godot
 
-echo Building Godot...
-py -3.13 -m SCons platform=windows dev_mode=yes accesskit=no d3d12=no
-
-if %errorlevel% neq 0 (
-    echo Build failed.
-    pause
-    exit /b
-)
-
-echo Build finished. Launching Godot...
+py -3.13 -m SCons platform=windows dev_mode=yes accesskit=no
 
 start "" "bin\godot.windows.editor.x86_64.exe"
-
-exit
